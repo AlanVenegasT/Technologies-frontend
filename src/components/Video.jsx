23085ec3143fb8d21b22165f170fa0abb9fdd233
AlanVenegasT <alan4444v@gmail.com>
2023-01-11 16:02:42 -0600

    Resadefcarr2v

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -8,22 +8,22 @@ export default function Video() {
       <div className="mx-auto lg:max-w-7xl  ">
         <div className="  lg:grid  lg:grid-cols-2 lg:gap-4  md:grid  md:grid-cols-2 md:gap-4  flex-col">
           <div className=" row-span-2 row-start-1 row-end-4  bg-[#27282D] rounded-md lg:mr-4 lg:ml-8 md:mr-8 md:ml-8 mr-4 ml-4   ">
-            <h2 className="lg:text-6xl md:text-4xl text-2xl  lg:ml-8 lg:mr-8 lg:mt-4 md:ml-5 md:mr-8 md:mt-6 mr-4 mt-3 ml-5 pt-12 px-10 font-bold text-white lg:leading-[60px] md:leading-[50px] leading-[24px] ">
+            <h2 className="lg:text-6xl md:text-4xl text-2xl  lg:ml-8 lg:mr-8 lg:mt-4 md:ml-5 md:mr-8 md:mt-2 mr-4 mt-3 ml-5 pt-12 px-10 font-bold text-white lg:leading-[60px] md:leading-[50px] leading-[24px] ">
               La mejor educacion en línea a tu alcance
             </h2>
-            <h3 className="lg:text-xl lg:ml-8 lg:mr-8 lg:mt-4 md:text-sm md:ml-10 md:mr-8 md:mt-6 text-sm ml-5 mr-6 mt-4 pt-12 px-10 text-[#B3B3B3]">
+            <h3 className="lg:text-xl lg:ml-8 lg:mr-8 lg:mt-4 md:text-sm md:ml-3 md:mr-3 md:mt-3 text-sm ml-5 mr-6 mt-4 pt-12 px-10 text-[#B3B3B3]">
               Inscribete a clases practicas con mentorias personalizadas
               dictadas por quienes mas saben, y preparate para dar el siguiente
               paso en tu carrera profesional{" "}
             </h3>
             <img
-              className="lg:pb-5 pb-5 pt-5"
+              className="lg:pb-5 pb-5 pt-3"
               src="https://res.cloudinary.com/hdsqazxtw/image/upload/f_webp,q_auto/v1642798986/WWW/imgs/home/chat.png"
               alt=""
             />
           </div>
-          <div className=" row-span-2  bg-gradient-to-r from-[#EA92FD] via-[#EAC5B8] to-[#E6FA72] rounded-md lg:mt-16 lg:mr-4 lg:ml-8 md:mt-16 md:mr-8 md:ml-8 mt-6 mr-4 ml-4 ">
-            <h2 className="lg:text-4xl md:text-3xl text-xl font-bold lg:ml-8 lg:mt-6 md:ml-8 md:mt-10 ml-6 mt-8 pt-8 text-[#1F2023]">
+          <div className=" row-span-2  bg-gradient-to-r from-[#EA92FD] via-[#EAC5B8] to-[#E6FA72] rounded-md lg:mt-16 lg:mr-4 lg:ml-8 md:mt-4 md:mr-8 md:ml-8 mt-6 mr-4 ml-4 ">
+            <h2 className="lg:text-4xl md:text-3xl text-xl font-bold lg:ml-8 lg:mt-6 md:ml-8 md:mt-1 ml-6 mt-8 pt-8 text-[#1F2023]">
               Tutores
             </h2>
             <h3 className="lg:ml-8 lg:mr-8 lg:mt-8 lg:mb-8 lg:text-xl md:ml-8 md:mr-8 md:mt-4 md:mb-8 md:text-sm ml-4 pt-0 px-2 mr-4 mt-2 mb-4 pb-5 text-sm text-[#1F2023]">
@@ -32,7 +32,7 @@ export default function Video() {
             </h3>
           </div>
           <div className="row-start-1 row-end-6 md:row-start-1 md:lg:row-end-6 lg:row-start-1   bg-[#27282D] rounded-md lg:mr-8 lg:ml-4 lg:mt-0 md:mt-0 md:mr-8 mr-4  ml-4 mt-0 ">
-            <div className="h-1/2 lg:flex lg:flex-row flex flex-row  lg:ml-8 lg:mr-8 lg:mt-10 lg:mb-0 lg:pb-20  md:ml-5 md:mr-5 md:mt-10 md:mb-8 ml-5 mr-5 mt-10 mb-8 pt-5 overflow-hidden ">
+            <div className="h-1/2 lg:flex lg:flex-row flex flex-row  lg:ml-8 lg:mr-8 lg:mt-10 lg:mb-0 lg:pb-20  md:ml-5 md:mr-5 md:mt-5 md:mb-0 md:pb-10 ml-5 mr-5 mt-10 mb-8 pt-5 overflow-hidden ">
               <div className=" basis-1/3 bg-[#202226] mx-auto  overflow-hidden  ">
                 {" "}
                 {/*carrusel 1 */}
@@ -189,7 +189,7 @@ export default function Video() {
             </div>
 
             <div className="h-1/2">
-              <h2 className="lg:text-4xl md:text-2xl  text-xl  font-bold lg:ml-8 lg:mt-10 md:ml-8 md:mt-10 ml-4 mt-5 px-5 text-white">
+              <h2 className="lg:text-4xl md:text-2xl  text-xl  font-bold lg:ml-8 lg:mt-10 md:ml-8 md:mt-0 ml-4 mt-5 px-5 text-white">
                 Profesores expertos
               </h2>
               <h3 className="lg:text-xl lg:ml-8 lg:mr-8 lg:mt-6 md:text-sm md:ml-8 md:mr-8 md:mt-6 text-sm ml-4 mr-4 mt-4 px-5 pb-28 text-[#B3B3B3]">
